d2ad81877a152a4cd023df48c7183c93172e4f9f
Franco <ufrancis186@gmail.com>
2025-11-02 04:09:07 +0100

    z

--- src/Herosection.jsx
+++ src/Herosection.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 function Herosection(){
     return(
-        <div className="mt-20">
+        <div className="flex flex-col items-center gap-7 mt-20">
             <p className="md:text-6xl text-white font-extrabold md:max-w-[600px] text-center">Manage your Labs with Ease</p>
 
-            
+            <div className="flex flex-row gap-5">
+                <p className="text-white font-bold border-2 border-white p-3 px-14 hover:bg-gray-200 transition-colors duration-500 hover:cursor-pointer">Learn More</p>
+                <p className="flex justify-center items-center font-bold w-[200px] h-[50px] bg-white hover:scale-110 transition-transform duration-500 hover:cursor-pointer">Get Started </p>
+            </div>
         </div>
     );
 }
